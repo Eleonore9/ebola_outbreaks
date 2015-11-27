1d474e4acbf5f80aa0c4d8a4bb70ab52b96bb404
Eleonore9 <eleonore.mayola@ymail.com>
2015-11-27 00:18:23 +0000

    WIP: trying to use cartodb api via cartodb-python

--- script/cartodb_data.py
+++ script/cartodb_data.py
@@ -1,8 +1,13 @@
-from cartodb import CartoDBAPIKey, CartoDBException, FileImport
+#!/home/eleonore/virtenvs/cartodb/bin/python2.7
+# -*- coding: utf-8 -*-
+import sys
+from cartodb import CartoDBAPIKey, CartoDBException
 
-API_KEY ='YOUR_CARTODB_API_KEY'
-cartodb_domain = 'YOUR_CARTODB_DOMAIN'
-cl = CartoDBAPIKey(API_KEY, cartodb_domain)
-
-fi = FileImport("test.csv", cl)
-fi.run()
+if __name__ == "__main__":
+    api_key, cartodb_domain = sys.argv[1], sys.argv[2]
+    cartodb_api = CartoDBAPIKey(api_key, cartodb_domain)
+    import_data = cartodb_api.req("https://" + cartodb_domain +
+                                  ".cartodb.com/api/v2/sql?q=", http_method="GET",
+                                  files="ebola_outbreaks_before_2014")
+    import_data.run()
+    print import_data.success
